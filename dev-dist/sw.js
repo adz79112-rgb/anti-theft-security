2e5dee661139426782f38f7c090c8e47a9f71b08
adz79112-rgb <adz79112@gmail.com>
2026-09-12 10:22:49 +0100

    build: update index.html cache revision in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ln4bv99pg08"
+    "revision": "0.5epitq2e44o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
