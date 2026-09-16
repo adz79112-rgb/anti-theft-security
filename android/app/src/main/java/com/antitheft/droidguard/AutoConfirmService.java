91cac80bc9ec5efb924eaa4617e85f57fcc9e2f3
adz79112-rgb <adz79112@gmail.com>
2026-09-17 00:14:47 +0100

    build: update DroidGuard project assets
    Update the DroidGuard Android project zip files to the latest version and add necessary locale imports.

--- android/app/src/main/java/com/antitheft/droidguard/AutoConfirmService.java
+++ android/app/src/main/java/com/antitheft/droidguard/AutoConfirmService.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class AutoConfirmService extends AccessibilityService {
 
